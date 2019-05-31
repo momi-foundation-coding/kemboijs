d34fefcec8f7729ba4e966a811d11e56cd487145
me-x-mi <developerezrqn@gmail.com>
2019-05-31 19:08:44 +0300

    make kemboijs server starter

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+const kemboijs = require('./lib/kemboijs');
+
+// Expose new instance of kemboijs
+// module.exports = new kemboijs();
+
+/**
+ * To access new constructor for kemboijs
+ *  var kemboijs = require('kemboijs).kemboijs
+*/
+
+module.exports.kemboijs = kemboijs;
